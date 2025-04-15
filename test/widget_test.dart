6733fd8a8998fc0dfd189eb1fe3dbeb33e0e098f
jx_hu <jxh_hjx@163.com>
2025-04-15 16:47:16 +0800

    submit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:category_picker/main.dart';
+import '../examples/example_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
